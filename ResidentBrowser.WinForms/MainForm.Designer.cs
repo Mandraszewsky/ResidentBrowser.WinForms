b89887b50fe4bcba3cc0984d1539de26f1903356
Mandraszewsky <mandrah@o2.pl>
2025-01-14 16:21:57 +0100

    Clean Architecture layers added (Application, Domain, Infrastructure). With some basic main panel.

--- ResidentBrowser.WinForms/MainForm.Designer.cs
+++ ResidentBrowser.WinForms/MainForm.Designer.cs
@@ -28,17 +28,80 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
+        navigationPanel = new Panel();
+        residentsButton = new Button();
+        panel1 = new Panel();
+        exportButton = new Button();
+        navigationPanel.SuspendLayout();
         SuspendLayout();
+        // 
+        // navigationPanel
+        // 
+        navigationPanel.BackColor = Color.MidnightBlue;
+        navigationPanel.Controls.Add(exportButton);
+        navigationPanel.Controls.Add(residentsButton);
+        navigationPanel.Controls.Add(panel1);
+        navigationPanel.Dock = DockStyle.Left;
+        navigationPanel.Location = new Point(0, 0);
+        navigationPanel.Name = "navigationPanel";
+        navigationPanel.Size = new Size(200, 561);
+        navigationPanel.TabIndex = 0;
+        // 
+        // residentsButton
+        // 
+        residentsButton.BackColor = Color.MidnightBlue;
+        residentsButton.Dock = DockStyle.Top;
+        residentsButton.FlatAppearance.BorderSize = 0;
+        residentsButton.FlatStyle = FlatStyle.Flat;
+        residentsButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+        residentsButton.ForeColor = Color.White;
+        residentsButton.Location = new Point(0, 76);
+        residentsButton.Name = "residentsButton";
+        residentsButton.Size = new Size(200, 75);
+        residentsButton.TabIndex = 1;
+        residentsButton.Text = "Residents";
+        residentsButton.UseVisualStyleBackColor = false;
+        // 
+        // panel1
+        // 
+        panel1.BackColor = Color.MidnightBlue;
+        panel1.Dock = DockStyle.Top;
+        panel1.Location = new Point(0, 0);
+        panel1.Name = "panel1";
+        panel1.Size = new Size(200, 76);
+        panel1.TabIndex = 2;
+        // 
+        // exportButton
+        // 
+        exportButton.BackColor = Color.MidnightBlue;
+        exportButton.Dock = DockStyle.Top;
+        exportButton.FlatAppearance.BorderSize = 0;
+        exportButton.FlatStyle = FlatStyle.Flat;
+        exportButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+        exportButton.ForeColor = Color.White;
+        exportButton.Location = new Point(0, 151);
+        exportButton.Name = "exportButton";
+        exportButton.Size = new Size(200, 75);
+        exportButton.TabIndex = 3;
+        exportButton.Text = "Exports";
+        exportButton.UseVisualStyleBackColor = false;
         // 
         // MainForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
+        ClientSize = new Size(884, 561);
+        Controls.Add(navigationPanel);
         Name = "MainForm";
         Text = "Resident Browser";
+        navigationPanel.ResumeLayout(false);
         ResumeLayout(false);
     }
 
     #endregion
+
+    private Panel navigationPanel;
+    private Button residentsButton;
+    private Panel panel1;
+    private Button exportButton;
 }
